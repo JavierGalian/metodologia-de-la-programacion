d10791f84a4fa9bdb50f0ed605c1701ab755612b
Javier Rodrigo Galian <javier.galian.1997@gmail.com>
2022-09-13 20:34:59 -0300

    modificacion TP5

--- TP5/TP5eje1.py
+++ TP5/TP5eje1.py
@@ -1,3 +1,40 @@
-with open("vehiculos.txt") as file_object:
-    leer = file_object.read()
-    print(leer.rstrip())
+from io import open
+
+vehiculos = []
+#fichero = open ('TP5/datos/vehiculos.txt','r',encoding="utf-8")
+#print(fichero.read())
+#fichero.close()
+
+#with open("TP5/datos/vehiculos.txt" , 'r' , encoding="utf-8") as archivo:
+ #   for linea in archivo:
+  #      vehiculo = [linea.rstrip("\n").split(";") for linea in archivo] 
+        #lineas = [linea.split() for linea in archivo]
+#for linea in vehiculo:
+ #   print(linea)
+archivo = open ('TP5/datos/vehiculos.txt','r',encoding="utf-8")
+for linea in archivo:
+    vehiculo=linea.rstrip("\n").split(";")
+    vehiculos.append(vehiculo)
+
+print(vehiculos)
+
+
+
+
+#veh = ast.literal_eval(vehiculos)
+#print(veh)
+
+
+
+#def cargarVehiculos(archivo):
+ #   listaVehiculos = []
+  #  for linea in archivo:
+   #     vehiculo = linea.rstrip("\n").split(";") 
+
+
+#salida = []
+#with open('archivo.txt', 'r') as f:
+ #   lineas = [linea.split() for linea in f]
+
+#for linea in lineas:
+ #   print(linea)
